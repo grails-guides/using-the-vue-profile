e1322b5c862d5dcc9dc7d24ba9f6cb71af1798b6
Sergio del Amo <sergio.delamo@softamo.com>
2019-01-10 16:07:44 +0100

    Change order

--- complete/runtests.sh
+++ complete/runtests.sh
@@ -4,24 +4,18 @@ EXIT_STATUS=0
 
 start=`date +%s`
 
-echo "Starting client and server"
-./gradlew bootRun -parallel --console=plain &
-PID1=$!
-
-echo "Waiting 4 seconds  for client and server to start"
-sleep 40
-
-echo "Executing tests"
-
 ./gradlew -Dgeb.env=chromeHeadless server:check --console=plain || EXIT_STATUS=$?
 
 if [ $EXIT_STATUS -ne 0 ]; then
   exit $EXIT_STATUS
 fi
 
-if [ $EXIT_STATUS -ne 0 ]; then
-  exit $EXIT_STATUS
-fi
+echo "Starting client and server"
+./gradlew bootRun -parallel --console=plain &
+PID1=$!
+
+echo "Waiting 40 seconds  for client and server to start"
+sleep 40
 
 ./gradlew --rerun-tasks -Dgeb.env=chromeHeadless functional-tests:test --console=plain || EXIT_STATUS=$?
 

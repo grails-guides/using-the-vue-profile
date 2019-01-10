922d178f50b20bc7a9956190581c5e14d85ec078
sdelamo <sergio.delamo@softamo.com>
2019-01-10 11:03:46 +0100

    Run tests

---
+++ complete/runtests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+EXIT_STATUS=0
+
+start=`date +%s`
+
+if [[ $EXIT_STATUS -eq 0 ]]; then
+  echo "Starting client and server"
+  ./gradlew bootRun -parallel > /dev/null 2>&1 &
+  PID1=$!
+  echo "Waiting for client and server to start"
+  sleep 4
+  echo "Executing tests"
+  ./gradlew --rerun-tasks functional-tests:test --console=plain || EXIT_STATUS=$?
+  killall -9 java
+  killall node
+fi
+end=`date +%s`
+runtime=$((end-start))
+echo "execution took $runtime seconds"

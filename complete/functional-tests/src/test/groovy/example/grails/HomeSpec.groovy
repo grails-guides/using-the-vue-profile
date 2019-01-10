df545c90baab7f10114bef44459d5654a5967f6e
sdelamo <sergio.delamo@softamo.com>
2019-01-10 11:22:35 +0100

    Skip tests if apps are not running

--- complete/functional-tests/src/test/groovy/example/grails/HomeSpec.groovy
+++ complete/functional-tests/src/test/groovy/example/grails/HomeSpec.groovy
@@ -2,9 +2,11 @@ package example.grails
 
 import geb.Browser
 import geb.spock.GebSpec
+import spock.lang.Requires
 
 class HomeSpec extends GebSpec {
 
+    @Requires({ Utils.available('http://localhost:8080') && Utils.available('http://localhost:3000') })
     def "homage shows a list of available controllers"() {
         given:
         Browser browser = new Browser()
